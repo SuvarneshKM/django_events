74a1b79d3766fca1c9a552de22d4ecfaa89ce501
Suvarnesh K M <suvarnesh1729@gmail.com>
2021-05-29 15:45:00 +0530

    add event details

--- theevent/urls.py
+++ theevent/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import HomeView, EventDetailView
 
 urlpatterns = [
-   path('', views.home, name="home"),
+   # path('', views.home, name="home"),
+   path('', HomeView.as_view(), name="home"),
+   path('event/<int:pk>', EventDetailView.as_view(), name="event-detail" ),
 ]
--- theevent/views.py
+++ theevent/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+# def home(request):
+# 	return render(request, 'home.html', {})
+
+class HomeView(ListView):
+	model = Post
+	template_name = 'home.html'
 
 
-def home(request):
-	return render(request, 'home.html', {})
+class EventDetailView(DetailView):
+	model = Post
+	template_name = 'event_detail.html'
